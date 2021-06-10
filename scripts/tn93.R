7367a1fd1ab7ee9c0f6d008d590d11a0c04a4a84
heathergrant <heather.grant@ed.ac.uk>
2021-06-10 11:28:26 +0100

    changed for just one folder at a time. Refers to folder with mafft windows made already

--- scripts/tn93.R
+++ scripts/tn93.R
@@ -3,30 +3,23 @@ library(ape)
 #library(strataG)
 library(parallel)
 
-setwd('/Users/abayomi/Desktop/new_protocol/')
-mylist = c("200", "100", "50")
+setwd('')
 
-for (i in mylist) {
+files <- list.files(paste0("mafft/"))
+
+mclapply(files, function(f) {
+  my_align <- read.alignment(file = paste0("mafft/", f), format='fasta')
+  y <- as.DNAbin(my_align)
+  tn93 <- dist.dna(y, model = "TN93", variance = FALSE,
+                   gamma = FALSE, pairwise.deletion = FALSE,
+                   base.freq = NULL, as.matrix = FALSE)
   
-  for (j in seq(1, 10, 1)) {
-    
-    files <- list.files(paste0("./ref_rec_10/", i, "/", j, "/mafft/"))
-    
-    mclapply(files, function(f) {
-      my_align <- read.alignment(file = paste0("./ref_rec_10/", i, "/", j, "/mafft/", f), format='fasta')
-      y <- as.DNAbin(my_align)
-      tn93 <- dist.dna(y, model = "TN93", variance = FALSE,
-                       gamma = FALSE, pairwise.deletion = FALSE,
-                       base.freq = NULL, as.matrix = FALSE)
-      
-      write.table(data.frame(as.matrix(tn93)), 
-                  file = paste0("./ref_rec_10/", i, "/", j, "/tn93/", f, ".tn93.csv"),
-                  append = F, 
-                  quote = FALSE, 
-                  sep = ",", 
-                  col.names = NA)
-    },
-    mc.cores = 8
-    )
-  }
-}
+  write.table(data.frame(as.matrix(tn93)), 
+              file = paste0("tn93/", f, ".tn93.csv"), 
+              append = F, 
+              quote = FALSE, 
+              sep = ",", 
+              col.names = NA)
+},
+mc.cores = 8
+)
