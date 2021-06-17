151d496d2183f45ec8b0873b6d13a93931255503
heathergrant <heather.grant@ed.ac.uk>
2021-06-17 14:52:00 +0100

    Now can stitch together one community frankenstein seq. Now having trouble making a consensus sequence with too many blanks

--- scripts/make_frankenstein_genomes.R
+++ scripts/make_frankenstein_genomes.R
@@ -35,32 +35,63 @@ for(i in 1:ncol(df)){
   k_along[,i]<-length(unique(df[,i]))
 }
 k_along
-
-#okay, let's look at cluster 1 in window 1 and get a central node? graph concept of centrality 
-k1w1<-df[,1][df[,1]=='1'] #only 5 sequences 
-?igraph_closeness
-df[,1]=='1'
-names(k1w1)
-#files[1]#
-#x <- read.table(files[1], sep=',', header=T, na.strings=c("NA", "NaN"), row.names=1)
-#x1<-x[names(k1w1),names(k1w1)]
-#library(igraph)
-#y<-graph_from_data_frame(x1, directed = TRUE, vertices = NULL)
-#plot(y)
-#x1
-#closeness(y, vids = V(y), mode = c("out", "in", "all",
-#                                           "total"), weights = NULL, normalized = FALSE)
-#not sure this works very well
-
-#which genome of this lot has the most '1's elsewhere in the genome? 
-mem1<-df[names(k1w1),]
-counts<-c()
-for(c in 1:length(rownames(mem1))){
-  counts<-c(counts, sum(mem1[c,]=='1'))
-}
-frank<-rownames(mem1)[which(counts==max(counts))] #4th sequence is best representative because it has other windows (31) that are also this community 
-#"Ref.G.PT.x.PT2695.AY612637"
 library(ape)
-dna.1<- read.dna(files.dna[1], format='fasta')
-frank.dna<-dna.1[frank,]
 
+setwd("../Frankenstein/")
+
+#make dna object to start off with. 
+starter<-read.dna("../ref_genomes.afa", format='fasta')
+starter<-starter[1,]#jsut the first one
+library(seqinr)
+write.fasta(rep('-', dim(starter)[2]), 'blank', 'blank-dash.fasta', open = "w", nbchar = 60, as.string = FALSE)
+blank<-read.dna("blank-dash.fasta", format='fasta')
+numWindows <- seq(1,((length(starter[1,])+500)%/%100)*100-500+1,by=100)-1
+#starter[,1]<-#list(rep("n", 3))# dim(starter)[2]
+#loop through windows - let's just make community 1 to begin with 
+Frankenstein<-starter
+k<-1 #first community 
+for(w in 1:ncol(df)){
+  kw<-df[,w][df[,1]==k] #only 5 sequences 
+  #which genome of this lot has the most '1's elsewhere in the genome? 
+  mem1<-df[names(kw),] #subset to just ones of interest 
+  counts<-c()
+  for(c in 1:length(rownames(mem1))){
+    counts<-c(counts, sum(mem1[c,]==k))
+  }
+  top<-which(counts==max(counts))
+  top<-top[sample(length(top), 1)] #pick one if there's more than one (will have to revisit this, quick fix)
+  frank<-rownames(mem1)[top] #4th sequence is best representative because it has other windows (31) that are also this community 
+  dna.1<- read.dna(files.dna[w], format='fasta')
+  frank.dna<-dna.1[frank,]
+  rownames(frank.dna)<-paste0("community_", k)
+  #some windows are 503 base pairs long! Presumably because of re-maffting them. 
+  frank.dna<-frank.dna[,1:500]
+  start<- numWindows[w]    #       or #100*(w+1)-200#start 
+  end<- numWindows[w+5]+1   #      or100*(w+1)+300 #end
+  blank1<-blank[,0:start]
+  rownames(blank1)<-paste0("community_", k)
+  blank2<-blank[,end:dim(starter)[2]]
+  rownames(blank2)<-paste0("community_", k)
+  rownames(frank.dna)<-paste0("community_", k)
+  #stitch blank + window + blank sequence to make an alignment of equal length Frankenstein to make cons
+  record.dna<-cbind.DNAbin(blank1,frank.dna)
+  record.dna<-cbind.DNAbin(record.dna, blank2)
+  #}
+  Frankenstein<-rbind.DNAbin(Frankenstein, record.dna)
+  
+}
+image(Frankenstein)
+Frankenstein<-Frankenstein[-1,]#remove starter seq 
+Frankenstein_condensed<-Frankenstein[1:5,]
+for(f in 1:dim(Frankenstein[2])){
+  nt<-unique(unlist(as.character(Frankenstein[,f])))
+  for(n in 1:length(nt)){
+  Frankenstein_condensed[n,f]<-as.character(nt[n])
+  }
+}
+#install.packages("DECIPHER")
+#Frankenstein<-as.matrix.alignment(Frankenstein)
+write.dna(Frankenstein, "Community1.fasta", format='fasta')
+matailn<-read.alignment("Community1.fasta", format='fasta')
+com1<-consensus(matailn, method =  "threshold", warn.non.IUPAC = FALSE, type = "DNA", threshold = 0)
+com1
